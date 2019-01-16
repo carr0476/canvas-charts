200e48b48878c161a4f698176f0d291bab1f9119
carr0476 <carr0476@algonquinlive.com>
2019-01-16 15:24:37 -0500

    added fetch call

--- main.js
+++ main.js
@@ -1,6 +1,21 @@
-let canvas, context;
 
-document.addEventListener('DOMContentLoaded', ()=>{
-    canvas = document.getElementById('canvas');
-    context = canvas.getContext('2d');
+document.addEventListener('DOMContentLoaded', function () {
+    let canvasP = document.getElementById("pie")
+    let canvasB = document.getElementById("bar")
+    //fetching the json data
+    console.log("executing Fetch")
+    fetch('./Data.json')
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            let pieData = data
+            console.log(data)
+        })
+        .catch(function (err) {
+            console.log("Error: ", err.message)
+        });
+
+        pieData[id].title
+
 })
