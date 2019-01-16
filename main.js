d737b4931807e991a03f7aa1c3d557a0e783fb93
carr0476 <carr0476@algonquinlive.com>
2019-01-16 17:07:34 -0500

    namespaced project modified json file and varios other changes

--- main.js
+++ main.js
@@ -1,7 +1,29 @@
+//Justin Carroll
+//carr0476
+//canvas charts assignment
 
 document.addEventListener('DOMContentLoaded', function () {
+
+    //add all the needed alemets of the canvas
     let canvasP = document.getElementById("pie")
+    let ctxP = canvasP.getContext("2d");
     let canvasB = document.getElementById("bar")
+    let ctxB = canvasB.getContext("2d");
+    var cxP = canvasP.width / 2;
+    var cyP = canvasP.height / 2;
+    var cxB = canvasB.width / 2;
+    var cyB = canvasB.height / 2;
+    let radius = 120;
+    let startAngle =0;
+
+    console.log("Pie chart", canvasP)
+    console.log("Bar Graph", canvasB)
+
+    //executing fetch
+    fetchData()
+})
+
+function fetchData() {
     //fetching the json data
     console.log("executing Fetch")
     fetch('./Data.json')
@@ -9,13 +31,29 @@ document.addEventListener('DOMContentLoaded', function () {
             return response.json();
         })
         .then(function (data) {
-            let pieData = data
+            //passing the fetched data to the filter function
+            getTotal(data)
+            makePie(data)
             console.log(data)
         })
         .catch(function (err) {
             console.log("Error: ", err.message)
         });
+}
 
-        pieData[id].title
+function getTotal(data) {
+    //gets the sum of the number in the json file
+let flavour= 0;
+    for(let i=0; i<data.length; i++){
+        // console.log(data[i].title)
+        flavour= flavour + Number(data[i].flavour)
+    }
+console.log("total is", flavour)
+}
 
-})
+function makePie(data){
+    data.forEach(pie => {
+        console.log("pie")
+    });
+}
+// data[id].title
